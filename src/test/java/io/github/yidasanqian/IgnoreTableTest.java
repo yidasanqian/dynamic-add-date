51f07861097ff949e68f02f64417627ea8d3a34d
yidasanqian <hzclyu@tairanchina.com>
2018-01-29 15:50:50 +0800

    添加单元测试

---
+++ src/test/java/io/github/yidasanqian/IgnoreTableTest.java
@@ -0,0 +1,93 @@
+package io.github.yidasanqian;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IgnoreTableTest {
+
+    @Test
+    public void testMatchesIgnoreTable() {
+        String tableName = "userAuthc";
+        String ignore = "^user.*, ^permission.*";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable2() {
+        String tableName = "user";
+        String ignore = "^user.*, ^permission.*";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable2 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable3() {
+        String tableName = "user";
+        String ignore = "^user.*, ^permission.*, ";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable3 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable4() {
+        String tableName = "user";
+        String ignore = "^user.*";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable4 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable5() {
+        String tableName = "user";
+        String ignore = "";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable5 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable6() {
+        String tableName = "user";
+        String ignore = "user,,permission";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable6 匹配");
+            }
+        }
+    }
+
+    @Test
+    public void testMatchesIgnoreTable7() {
+        String tableName = "user_role";
+        String ignore = "user.*,, permission";
+        List<String> ignoreTableList = Arrays.asList(ignore.split(","));
+        for (String ignoreTable : ignoreTableList) {
+            if (tableName.matches(ignoreTable)) {
+                System.out.println("IgnoreTableTest.testMatchesIgnoreTable7 匹配");
+            }
+        }
+    }
+}
